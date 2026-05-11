16e8810b48509740e9fcc70b6e908fd69e8d8f6f
Gihido <matuhinaleks058@gmail.com>
2026-05-11 11:14:14 +0400

    Add collect square coin flow and pixel UI polish

--- src/ServerScriptService/MainServer.server.lua
+++ src/ServerScriptService/MainServer.server.lua
@@ -10,6 +10,7 @@ local REMOTE_NAMES = {
 	"BuyUpgrade",
 	"UpgradeResult",
 	"SyncPlayerData",
+	"CollectZoneState",
 }
 
 local function getOrCreateRemotes()
@@ -23,7 +24,7 @@ local function getOrCreateRemotes()
 
 	local remotes = {}
 
-	for _, remoteName in REMOTE_NAMES do
+	for _, remoteName in ipairs(REMOTE_NAMES) do
 		local remote = remotesFolder:FindFirstChild(remoteName)
 
 		if not remote then
