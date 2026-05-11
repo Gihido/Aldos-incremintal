a5e4a394bffc7395e342fe1a73a0f92ed5475c15
Gihido <matuhinaleks058@gmail.com>
2026-05-11 09:10:10 +0400

    Fix upgrade buttons and scroll safety

--- src/ReplicatedStorage/Shared/UIAssetConfig.module.lua
+++ src/ReplicatedStorage/Shared/UIAssetConfig.module.lua
@@ -1,18 +1,48 @@
 local UIAssetConfig = {
 	UpgradeCards = {
 		CoinGain = {
+			CardBackground = "rbxassetid://0",
 			BackgroundImage = "rbxassetid://0",
 			IconImage = "rbxassetid://0",
+
+			ValueBoxBackground = "rbxassetid://0",
+			PriceBoxBackground = "rbxassetid://0",
+
+			BuyButtonImage = "rbxassetid://0",
+			BuyMaxButtonImage = "rbxassetid://0",
+			BuyButtonBackground = "rbxassetid://0",
+			BuyMaxButtonBackground = "rbxassetid://0",
+
 			PurchaseEffectImage = "rbxassetid://0",
 		},
 		MultiCoins = {
+			CardBackground = "rbxassetid://0",
 			BackgroundImage = "rbxassetid://0",
 			IconImage = "rbxassetid://0",
+
+			ValueBoxBackground = "rbxassetid://0",
+			PriceBoxBackground = "rbxassetid://0",
+
+			BuyButtonImage = "rbxassetid://0",
+			BuyMaxButtonImage = "rbxassetid://0",
+			BuyButtonBackground = "rbxassetid://0",
+			BuyMaxButtonBackground = "rbxassetid://0",
+
 			PurchaseEffectImage = "rbxassetid://0",
 		},
 		MaxSpawnCoins = {
+			CardBackground = "rbxassetid://0",
 			BackgroundImage = "rbxassetid://0",
 			IconImage = "rbxassetid://0",
+
+			ValueBoxBackground = "rbxassetid://0",
+			PriceBoxBackground = "rbxassetid://0",
+
+			BuyButtonImage = "rbxassetid://0",
+			BuyMaxButtonImage = "rbxassetid://0",
+			BuyButtonBackground = "rbxassetid://0",
+			BuyMaxButtonBackground = "rbxassetid://0",
+
 			PurchaseEffectImage = "rbxassetid://0",
 		},
 	},
