9dd585c9a6d55440582f32e6bf12fda09df47b8d
Gihido <matuhinaleks058@gmail.com>
2026-05-11 21:05:25 +0400

    Polish inventory mobile text and buff visuals

--- src/ReplicatedStorage/Shared/UIAssetConfig.module.lua
+++ src/ReplicatedStorage/Shared/UIAssetConfig.module.lua
@@ -1,18 +1,55 @@
 local UIAssetConfig = {
+	Fonts = {
+		Main = Enum.Font.Arcade,
+	},
+
 	UpgradeCards = {
 		CoinGain = {
+			CardBackground = "rbxassetid://0",
 			BackgroundImage = "rbxassetid://0",
 			IconImage = "rbxassetid://0",
+
+			ValueBoxBackground = "rbxassetid://0",
+			PriceBoxBackground = "rbxassetid://0",
+
+			BuyButtonImage = "rbxassetid://0",
+			BuyMaxButtonImage = "rbxassetid://0",
+			BuyButtonBackground = "rbxassetid://0",
+			BuyMaxButtonBackground = "rbxassetid://0",
+
+			TooltipBackground = "rbxassetid://0",
 			PurchaseEffectImage = "rbxassetid://0",
 		},
 		MultiCoins = {
+			CardBackground = "rbxassetid://0",
 			BackgroundImage = "rbxassetid://0",
 			IconImage = "rbxassetid://0",
+
+			ValueBoxBackground = "rbxassetid://0",
+			PriceBoxBackground = "rbxassetid://0",
+
+			BuyButtonImage = "rbxassetid://0",
+			BuyMaxButtonImage = "rbxassetid://0",
+			BuyButtonBackground = "rbxassetid://0",
+			BuyMaxButtonBackground = "rbxassetid://0",
+
+			TooltipBackground = "rbxassetid://0",
 			PurchaseEffectImage = "rbxassetid://0",
 		},
 		MaxSpawnCoins = {
+			CardBackground = "rbxassetid://0",
 			BackgroundImage = "rbxassetid://0",
 			IconImage = "rbxassetid://0",
+
+			ValueBoxBackground = "rbxassetid://0",
+			PriceBoxBackground = "rbxassetid://0",
+
+			BuyButtonImage = "rbxassetid://0",
+			BuyMaxButtonImage = "rbxassetid://0",
+			BuyButtonBackground = "rbxassetid://0",
+			BuyMaxButtonBackground = "rbxassetid://0",
+
+			TooltipBackground = "rbxassetid://0",
 			PurchaseEffectImage = "rbxassetid://0",
 		},
 	},
@@ -52,6 +89,79 @@ local UIAssetConfig = {
 	Leaderboard = {
 		BackgroundImage = "rbxassetid://0",
 	},
+
+	CollectZone = {
+		BackgroundImage = "rbxassetid://0",
+	},
+
+	Inventory = {
+		Main = {
+			ToggleButton = "rbxassetid://0",
+			Background = "rbxassetid://0",
+			CloseButton = "rbxassetid://0",
+
+			ItemsTabButton = "rbxassetid://0",
+			PassivesTabButton = "rbxassetid://0",
+
+			ItemSlotBackground = "rbxassetid://0",
+			ItemInfoBackground = "rbxassetid://0",
+			TooltipBackground = "rbxassetid://0",
+			BuffTooltipBackground = "rbxassetid://0",
+
+			ActivateButtonBackground = "rbxassetid://0",
+			DeleteButtonBackground = "rbxassetid://0",
+
+			ActiveBuffsBackground = "rbxassetid://0",
+			BuffSlotBackground = "rbxassetid://0",
+		},
+
+		Items = {
+			Carrot = {
+				Icon = "rbxassetid://0",
+				SlotBackground = "rbxassetid://0",
+				InfoBackground = "rbxassetid://0",
+				TooltipBackground = "rbxassetid://0",
+				BuffTooltipBackground = "rbxassetid://0",
+				BuffIcon = "rbxassetid://0",
+				ActivateButtonBackground = "rbxassetid://0",
+				DeleteButtonBackground = "rbxassetid://0",
+			},
+
+			Cucumber = {
+				Icon = "rbxassetid://0",
+				SlotBackground = "rbxassetid://0",
+				InfoBackground = "rbxassetid://0",
+				TooltipBackground = "rbxassetid://0",
+				BuffTooltipBackground = "rbxassetid://0",
+				BuffIcon = "rbxassetid://0",
+				ActivateButtonBackground = "rbxassetid://0",
+				DeleteButtonBackground = "rbxassetid://0",
+			},
+
+			Tomato = {
+				Icon = "rbxassetid://0",
+				SlotBackground = "rbxassetid://0",
+				InfoBackground = "rbxassetid://0",
+				TooltipBackground = "rbxassetid://0",
+				BuffTooltipBackground = "rbxassetid://0",
+				BuffIcon = "rbxassetid://0",
+				ActivateButtonBackground = "rbxassetid://0",
+				DeleteButtonBackground = "rbxassetid://0",
+			},
+
+			Corn = {
+				Icon = "rbxassetid://0",
+				SlotBackground = "rbxassetid://0",
+				InfoBackground = "rbxassetid://0",
+				TooltipBackground = "rbxassetid://0",
+				BuffTooltipBackground = "rbxassetid://0",
+				BuffIcon = "rbxassetid://0",
+				ActivateButtonBackground = "rbxassetid://0",
+				DeleteButtonBackground = "rbxassetid://0",
+			},
+		},
+	},
+
 }
 
 return UIAssetConfig
