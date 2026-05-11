c4bf7c9d8741327d44e983f915f5183322f1b7d4
Gihido <matuhinaleks058@gmail.com>
2026-05-11 15:29:28 +0400

    Update UIAssetConfig.module.lua

--- src/ReplicatedStorage/Shared/UIAssetConfig.module.lua
+++ src/ReplicatedStorage/Shared/UIAssetConfig.module.lua
@@ -5,51 +5,51 @@ local UIAssetConfig = {
 
 	UpgradeCards = {
 		CoinGain = {
-			CardBackground = "rbxassetid://0",
-			BackgroundImage = "rbxassetid://0",
-			IconImage = "rbxassetid://0",
+			CardBackground = "rbxassetid://109166926039423",
+			BackgroundImage = "rbxassetid://109166926039423",
+			IconImage = "rbxassetid://92543143637275",
 
-			ValueBoxBackground = "rbxassetid://0",
-			PriceBoxBackground = "rbxassetid://0",
+			ValueBoxBackground = "rbxassetid://107495296396884",
+			PriceBoxBackground = "rbxassetid://107495296396884",
 
-			BuyButtonImage = "rbxassetid://0",
-			BuyMaxButtonImage = "rbxassetid://0",
-			BuyButtonBackground = "rbxassetid://0",
-			BuyMaxButtonBackground = "rbxassetid://0",
+			BuyButtonImage = "rbxassetid://107495296396884",
+			BuyMaxButtonImage = "rbxassetid://107495296396884",
+			BuyButtonBackground = "rbxassetid://107495296396884",
+			BuyMaxButtonBackground = "rbxassetid://107495296396884",
 
-			TooltipBackground = "rbxassetid://0",
+			TooltipBackground = "rbxassetid://107495296396884",
 			PurchaseEffectImage = "rbxassetid://0",
 		},
 		MultiCoins = {
-			CardBackground = "rbxassetid://0",
+			CardBackground = "rbxassetid://128820244886313",
 			BackgroundImage = "rbxassetid://0",
 			IconImage = "rbxassetid://0",
 
-			ValueBoxBackground = "rbxassetid://0",
-			PriceBoxBackground = "rbxassetid://0",
+			ValueBoxBackground = "rbxassetid://117696920918961",
+			PriceBoxBackground = "rbxassetid://117696920918961",
 
-			BuyButtonImage = "rbxassetid://0",
-			BuyMaxButtonImage = "rbxassetid://0",
-			BuyButtonBackground = "rbxassetid://0",
-			BuyMaxButtonBackground = "rbxassetid://0",
+			BuyButtonImage = "rbxassetid://1176969209189610",
+			BuyMaxButtonImage = "rbxassetid://117696920918961",
+			BuyButtonBackground = "rbxassetid://117696920918961",
+			BuyMaxButtonBackground = "rbxassetid://117696920918961",
 
-			TooltipBackground = "rbxassetid://0",
+			TooltipBackground = "rbxassetid://117696920918961",
 			PurchaseEffectImage = "rbxassetid://0",
 		},
 		MaxSpawnCoins = {
-			CardBackground = "rbxassetid://0",
+			CardBackground = "rbxassetid://137763950449782",
 			BackgroundImage = "rbxassetid://0",
 			IconImage = "rbxassetid://0",
 
-			ValueBoxBackground = "rbxassetid://0",
-			PriceBoxBackground = "rbxassetid://0",
+			ValueBoxBackground = "rbxassetid://134992706671850",
+			PriceBoxBackground = "rbxassetid://134992706671850",
 
-			BuyButtonImage = "rbxassetid://0",
-			BuyMaxButtonImage = "rbxassetid://0",
-			BuyButtonBackground = "rbxassetid://0",
-			BuyMaxButtonBackground = "rbxassetid://0",
+			BuyButtonImage = "rbxassetid://134992706671850",
+			BuyMaxButtonImage = "rbxassetid://134992706671850",
+			BuyButtonBackground = "rbxassetid://134992706671850",
+			BuyMaxButtonBackground = "rbxassetid://134992706671850",
 
-			TooltipBackground = "rbxassetid://0",
+			TooltipBackground = "rbxassetid://134992706671850",
 			PurchaseEffectImage = "rbxassetid://0",
 		},
 	},
@@ -64,25 +64,25 @@ local UIAssetConfig = {
 
 	CoinPickupPopup = {
 		IconImage = "rbxassetid://0",
-		BackgroundImage = "rbxassetid://0",
+		BackgroundImage = "rbxassetid://124326451344705",
 	},
 
 	Notifications = {
 		Success = {
-			BackgroundImage = "rbxassetid://0",
-			IconImage = "rbxassetid://0",
+			BackgroundImage = "rbxassetid://112898706895909",
+			IconImage = "rbxassetid://75938736018356",
 		},
 		Limit = {
-			BackgroundImage = "rbxassetid://0",
-			IconImage = "rbxassetid://0",
+			BackgroundImage = "rbxassetid://100119440680374",
+			IconImage = "rbxassetid://116226379106030",
 		},
 		NotEnough = {
-			BackgroundImage = "rbxassetid://0",
-			IconImage = "rbxassetid://0",
+			BackgroundImage = "rbxassetid://100119440680374",
+			IconImage = "rbxassetid://116226379106030",
 		},
 		Error = {
-			BackgroundImage = "rbxassetid://0",
-			IconImage = "rbxassetid://0",
+			BackgroundImage = "rbxassetid://100119440680374",
+			IconImage = "rbxassetid://116226379106030",
 		},
 	},
 
