6d82ae74eacfd642cad7c61314217d9a695adb0c
Gihido <matuhinaleks058@gmail.com>
2026-05-12 18:16:52 +0400

    Update UIAssetConfig.module.lua

--- src/ReplicatedStorage/Shared/UIAssetConfig.module.lua
+++ src/ReplicatedStorage/Shared/UIAssetConfig.module.lua
@@ -89,15 +89,17 @@ local UIAssetConfig = {
 			BackgroundImage = "rbxassetid://100119440680374",
 			IconImage = "rbxassetid://116226379106030",
 		},
+
 		Claimed = {
-			BackgroundImage = "rbxassetid://0",
-			IconImage = "rbxassetid://0",
-			FallingCoinImage = "rbxassetid://0",
+			BackgroundImage = "rbxassetid://112898706895909",
+			IconImage = "rbxassetid://75938736018356",
+			FallingCoinImage = "rbxassetid://128844489893070",
 		},
+
 		SuccessClaim = {
-			BackgroundImage = "rbxassetid://0",
-			IconImage = "rbxassetid://0",
-			FallingCoinImage = "rbxassetid://0",
+			BackgroundImage = "rbxassetid://112898706895909",
+			IconImage = "rbxassetid://75938736018356",
+			FallingCoinImage = "rbxassetid://128844489893070",
 		},
 	},
 
@@ -111,11 +113,11 @@ local UIAssetConfig = {
 
 	Inventory = {
 		Main = {
-			ToggleButton = "rbxassetid://0",
-			Background = "rbxassetid://0",
+			ToggleButton = "rbxassetid://128844489893070",
+			Background = "rbxassetid://140022583234279",
 			CloseButton = "rbxassetid://0",
 
-			ItemsTabButton = "rbxassetid://0",
+			ItemsTabButton = "rbxassetid://128844489893070",
 			PassivesTabButton = "rbxassetid://0",
 			PackTraderTabButton = "rbxassetid://0",
 
@@ -132,81 +134,85 @@ local UIAssetConfig = {
 		},
 
 		PackTrader = {
-			Background = "rbxassetid://0",
-			TraderImage = "rbxassetid://0",
-			NormalTraderImage = "rbxassetid://0",
-			SuspiciousTraderImage = "rbxassetid://0",
-			AngryTraderImage = "rbxassetid://0",
+			Background = "rbxassetid://73926779494617",
 
-			DarkFadeImage = "rbxassetid://0",
-			MistFadeImage = "rbxassetid://0",
+			-- Старое поле оставлено для совместимости со старым кодом.
+			TraderImage = "rbxassetid://133730409830043",
 
-			DialogBackground = "rbxassetid://0",
+			-- 3 состояния торговца.
+			NormalTraderImage = "rbxassetid://133730409830043",
+			SuspiciousTraderImage = "rbxassetid://105828406599384",
+			AngryTraderImage = "rbxassetid://126618293548412",
+
+			-- Эффект входа: затемнение + туман.
+			DarkFadeImage = "rbxassetid://109166926039423",
+			MistFadeImage = "rbxassetid://109166926039423",
+
+			DialogBackground = "1",
 			AnswerButtonBackground = "rbxassetid://0",
 			PositiveAnswerButtonBackground = "rbxassetid://0",
 			NegativeAnswerButtonBackground = "rbxassetid://0",
 
-			OffersBackground = "rbxassetid://0",
-			OfferSlotBackground = "rbxassetid://0",
-			SoldOutBackground = "rbxassetid://0",
-			FallingCoinImage = "rbxassetid://0",
+			OffersBackground = "rbxassetid://91033843318571",
+			OfferSlotBackground = "rbxassetid://1",
+			SoldOutBackground = "rbxassetid://1",
+			FallingCoinImage = "rbxassetid://128844489893070",
 
-			BuyButtonBackground = "rbxassetid://0",
-			BackButtonBackground = "rbxassetid://0",
+			BuyButtonBackground = "rbxassetid://1",
+			BackButtonBackground = "rbxassetid://1",
 
-			RewardBackground = "rbxassetid://0",
-			RewardSlotBackground = "rbxassetid://0",
+			RewardBackground = "rbxassetid://1",
+			RewardSlotBackground = "rbxassetid://1",
 
-			TooltipBackground = "rbxassetid://0",
+			TooltipBackground = "rbxassetid://1",
 		},
 
 		Items = {
 			Carrot = {
-				Icon = "rbxassetid://0",
-				SlotBackground = "rbxassetid://0",
-				InfoBackground = "rbxassetid://0",
-				TooltipBackground = "rbxassetid://0",
-				BuffTooltipBackground = "rbxassetid://0",
-				BuffIcon = "rbxassetid://0",
-				ActivateButtonBackground = "rbxassetid://0",
-				DeleteButtonBackground = "rbxassetid://0",
+				Icon = "rbxassetid://93674418075080",
+				SlotBackground = "rbxassetid://99587245115312",
+				InfoBackground = "rbxassetid://102755677512720",
+				TooltipBackground = "rbxassetid://107590106110073",
+				BuffTooltipBackground = "rbxassetid://107590106110073",
+				BuffIcon = "rbxassetid://95282308527722",
+				ActivateButtonBackground = "rbxassetid://77203308839827",
+				DeleteButtonBackground = "rbxassetid://129408383648107",
 			},
 
 			Cucumber = {
-				Icon = "rbxassetid://0",
-				SlotBackground = "rbxassetid://0",
-				InfoBackground = "rbxassetid://0",
-				TooltipBackground = "rbxassetid://0",
-				BuffTooltipBackground = "rbxassetid://0",
-				BuffIcon = "rbxassetid://0",
-				ActivateButtonBackground = "rbxassetid://0",
-				DeleteButtonBackground = "rbxassetid://0",
+				Icon = "rbxassetid://88418733093660",
+				SlotBackground = "rbxassetid://102760269801253",
+				InfoBackground = "rbxassetid://133093722280631",
+				TooltipBackground = "rbxassetid://82336640510983",
+				BuffTooltipBackground = "rbxassetid://82336640510983",
+				BuffIcon = "rbxassetid://73492989732472",
+				ActivateButtonBackground = "rbxassetid://77203308839827",
+				DeleteButtonBackground = "rbxassetid://129408383648107",
 			},
 
 			Tomato = {
-				Icon = "rbxassetid://0",
-				SlotBackground = "rbxassetid://0",
-				InfoBackground = "rbxassetid://0",
-				TooltipBackground = "rbxassetid://0",
-				BuffTooltipBackground = "rbxassetid://0",
-				BuffIcon = "rbxassetid://0",
-				ActivateButtonBackground = "rbxassetid://0",
-				DeleteButtonBackground = "rbxassetid://0",
+				Icon = "rbxassetid://119354503617298",
+				SlotBackground = "rbxassetid://118896161283312",
+				InfoBackground = "rbxassetid://79636336699958",
+				TooltipBackground = "rbxassetid://136580105190791",
+				BuffTooltipBackground = "rbxassetid://136580105190791",
+				BuffIcon = "rbxassetid://81405686638766",
+				ActivateButtonBackground = "rbxassetid://77203308839827",
+				DeleteButtonBackground = "rbxassetid://129408383648107",
 			},
 
 			Corn = {
-				Icon = "rbxassetid://0",
-				SlotBackground = "rbxassetid://0",
-				InfoBackground = "rbxassetid://0",
-				TooltipBackground = "rbxassetid://0",
-				BuffTooltipBackground = "rbxassetid://0",
-				BuffIcon = "rbxassetid://0",
-				ActivateButtonBackground = "rbxassetid://0",
-				DeleteButtonBackground = "rbxassetid://0",
+				Icon = "rbxassetid://80010445836372",
+				SlotBackground = "rbxassetid://118896161283312",
+				InfoBackground = "rbxassetid://80078464428645",
+				TooltipBackground = "rbxassetid://126111932568663",
+				BuffTooltipBackground = "rbxassetid://126111932568663",
+				BuffIcon = "rbxassetid://93478402312137",
+				ActivateButtonBackground = "rbxassetid://77203308839827",
+				DeleteButtonBackground = "rbxassetid://129408383648107",
 			},
 		},
 	},
-
 }
 
 return UIAssetConfig
